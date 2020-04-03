a8dfc47fd37ad0fd6a577f68720f05a2edeca251
edegtev <evgeny89degtev@mail.ru>
2020-04-04 00:23:28 +0300

    Add Buy in HomeController

--- ControlPanel/Controllers/HomeController.cs
+++ ControlPanel/Controllers/HomeController.cs
@@ -1,10 +1,7 @@
 ﻿using ControlPanel.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using ControlPanel.Models;
 
 namespace ControlPanel.Controllers
 {
@@ -16,6 +13,22 @@ namespace ControlPanel.Controllers
             IEnumerable<Book> books = db.Books;
             ViewBag.Books = books;
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return "Спасибо, " + purchase.Person + ", за  покупку!";
         }
     }
 }
