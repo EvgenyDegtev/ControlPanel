fc91d620fa17946346455551feb178cf0f3bce3e
edegtev <evgeny89degtev@mail.ru>
2020-04-20 23:37:26 +0300

    Add display attribute to Agent, Group models

--- ControlPanel/Models/Agent.cs
+++ ControlPanel/Models/Agent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ControlPanel.Models
 {
@@ -9,18 +10,24 @@ namespace ControlPanel.Models
     {
         public int Id { get; set; }
 
+        [Display(Name="Имя")]
         public string Name { get; set; }
 
+        [Display(Name="Логин")]
         public string Login { get; set; }
 
+        [Display(Name="Алгоритм")]
         public int Algorithm { get; set; }
 
+        [Display(Name="Желаемый уровень обслуживания")]
         public bool IsAlgorithmAllowServiceLevel { get; set; }
 
+        [Display(Name ="Максимальная нагрузка")]
         public int WorkloadMaxContactsCount { get; set; }
 
         //Other properties
 
+        [Display(Name ="Активен")]
         public bool IsActive { get; set; }
     }
 }
--- ControlPanel/Models/Group.cs
+++ ControlPanel/Models/Group.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ControlPanel.Models
 {
@@ -9,10 +10,13 @@ namespace ControlPanel.Models
     {
         public int Id { get; set; }
 
+        [Display(Name ="Название")]
         public string Name { get; set; }
 
+        [Display(Name="Комментарий")]
         public string Description { get; set; }
 
+        [Display(Name="Активен")]
         public bool IsActive { get; set; }
     }
 }
