b9a35193058584d6253fc9076a6af364af0e4a2c
edegtev <evgeny89degtev@mail.ru>
2020-06-26 01:26:34 +0300

    Add ControlPanel.DAL project

---
+++ ControlPanel.DAL/Entities/Agent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace ControlPanel.DAL.Entities
+{
+    public class Agent
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Требуется поле Имя")]
+        [StringLength(100, ErrorMessage = "Максимальная длина - 100 символов")]
+        public string Name { get; set; }
+
+        [Display(Name = "Логин")]
+        [Required(ErrorMessage ="Требуется поле Логин")]
+        [StringLength(100, ErrorMessage = "Максимальная длина - 100 символов")]
+        [Remote(action:"CheckLoginUnique",controller:"Agents", AdditionalFields =nameof(Id))]
+        public string Login { get; set; }
+
+        [Display(Name = "Алгоритм")]
+        public int Algorithm { get; set; }
+
+        [Display(Name = "Желаемый уровень обслуживания")]
+        public bool IsAlgorithmAllowServiceLevel { get; set; }
+
+        [Display(Name = "Максимальная нагрузка")]
+        [Range(0,10,ErrorMessage ="Поле Максимальная нагрузка должно иметь значение между 0 и 10")]
+        public int WorkloadMaxContactsCount { get; set; }
+
+        //Other properties
+
+        [Display(Name = "Активен")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "Группа")]
+        public int? GroupId { get; set; }
+
+        public Group Group { get; set; }
+
+        public List<AgentToSkill> AgentToSkills { get; set; }
+    }
+}
---
+++ ControlPanel.DAL/Entities/Group.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace ControlPanel.DAL.Entities
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Группа")]
+        [Required(ErrorMessage = "Требуется поле Группа")]
+        [MaxLength(100,ErrorMessage = "Максимальная длина - 100 символов")]
+        [Remote(action: "CheckNameUnique", controller: "Groups", AdditionalFields = nameof(Id))]
+        public string Name { get; set; }
+
+        [Display(Name="Комментарий")]
+        [MaxLength(5000,ErrorMessage = "Максимальная длина - 5000 символов")]
+        public string Description { get; set; }
+
+        [Display(Name="Активен")]
+        public bool IsActive { get; set; }
+
+        public List<Agent> Agents { get; set; }
+    }
+}
