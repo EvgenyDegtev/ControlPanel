0e5de48b54f46af6c4c6a719976fd67185592887
edegtev <evgeny89degtev@mail.ru>
2020-09-30 00:59:47 +0300

    Add Async version to EFAgentRepository

--- ControlPanel/Abstract/IAgentRepository.cs
+++ ControlPanel/Abstract/IAgentRepository.cs
@@ -11,26 +11,47 @@ namespace ControlPanel.Abstract
     {
         IQueryable<Agent> Agents { get; }
 
+        Task<List<Agent>> GetAgentsAsync();
+
         IQueryable<Group> Groups { get; }
+
+        Task<List<Group>> GetGroupsAsync();
 
         IQueryable<Skill> Skills { get; }
 
+        Task<List<Skill>> GetSkillsAsync();
+
         IQueryable<Agent> AgentsIncludeGroup { get; }
+
+        Task<List<Agent>> GetAgentsIncludeGroupAsync();
 
         Agent FindAgentById(int id);
 
+        Task<Agent> FindAgentByIdAsync(int? id);
+
         Skill FindSkillById(int skillId);
+
+        Task<Skill> FindSkillByIdAsync(int? skillId);
 
         Agent FindAgentByIdIncludeGroup(int id);
 
+        Task<Agent> FindAgentByIdIncludeGroupAsync(int? id);
+
         Agent FindAgentByIdIncludeSkill(int id);
+
+        Task<Agent> FindAgentByIdIncludeSkillsAsync(int? id);
 
         IQueryable<AgentToSkill> FindAgentToSkillForAgentById(int agentId);
 
+        Task<List<AgentToSkill>> FindAgentToSkillForAgentByIdAsync(int? agentId);
 
         IQueryable<Agent> FindAgentsByLogin(string login);
 
+        Task<List<Agent>> FindAgentsByLoginAsync(string login);
+
         void Save();
+
+        Task SaveAsync();
 
         void Create(Agent agent);
 
@@ -40,12 +61,20 @@ namespace ControlPanel.Abstract
 
         void Delete(int id);
 
+        Task DeleteAsync(int id);
+
         IQueryable<Agent> SearchAgent(string searchString);
+
+        Task<List<Agent>> SearchAgentsAsync(string searchString);
 
         void CreateAgentToSkill(AgentToSkill agentToSkill);
 
         void DeleteAgentToSkill(int id);
 
+        Task DeleteAgentToSkillAsync(int id);
+
         IQueryable<AgentToSkill> FindAgentToSkills(int agentId, int skillId);
+
+        Task<List<AgentToSkill>> FindAgentToSkillsAsync(int agentId, int skillId);
     }
 }
--- ControlPanel/Concrete/EFAgentRepository.cs
+++ ControlPanel/Concrete/EFAgentRepository.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Data.Entity;
 using ControlPanel.Models;
 using ControlPanel.Abstract;
+using System.Threading.Tasks;
 
 namespace ControlPanel.Concrete
 {
@@ -22,6 +23,12 @@ namespace ControlPanel.Concrete
 
         }
 
+        public async Task<List<Agent>> GetAgentsAsync()
+        {
+            var agents = await context.Agents.ToListAsync();
+            return agents;
+        }
+
         public IQueryable<Skill> Skills
         {
             get
@@ -29,6 +36,12 @@ namespace ControlPanel.Concrete
                 var skills = context.Skills.AsQueryable();
                 return skills;
             }
+        }
+
+        public async Task<List<Skill>> GetSkillsAsync()
+        {
+            var skills = await context.Skills.ToListAsync();
+            return skills;
         }
 
         public IQueryable<Group> Groups
@@ -40,6 +53,12 @@ namespace ControlPanel.Concrete
             }
         }
 
+        public async Task<List<Group>> GetGroupsAsync()
+        {
+            var groups = await context.Groups.ToListAsync();
+            return groups;
+        }
+
         public IQueryable<Agent> AgentsIncludeGroup
         {
             get
@@ -49,9 +68,21 @@ namespace ControlPanel.Concrete
             }
         }
 
+        public async Task<List<Agent>> GetAgentsIncludeGroupAsync()
+        {
+            var agents = await context.Agents.Include(agent => agent.Group).ToListAsync();
+            return agents;
+        }
+
         public Agent FindAgentById (int id)
         {
             var agent = context.Agents.Find(id);
+            return agent;
+        }
+
+        public async Task<Agent> FindAgentByIdAsync(int? id)
+        {
+            var agent = await context.Agents.FindAsync(id);
             return agent;
         }
 
@@ -61,9 +92,21 @@ namespace ControlPanel.Concrete
             return skill;
         }
 
+        public async Task<Skill> FindSkillByIdAsync(int? skillId)
+        {
+            var skill = await context.Skills.FindAsync(skillId);
+            return skill;
+        }
+
         public Agent FindAgentByIdIncludeGroup(int id)
         {
             var agent = context.Agents.Include(agent => agent.Group).FirstOrDefault(agent => agent.Id == id);
+            return agent;
+        }
+ 
+        public async Task<Agent> FindAgentByIdIncludeGroupAsync(int? id)
+        {
+            var agent = await context.Agents.Include(agent => agent.Group).FirstOrDefaultAsync(agent => agent.Id == id);
             return agent;
         }
 
@@ -73,15 +116,33 @@ namespace ControlPanel.Concrete
             return agent;
         }
 
+        public async Task<Agent> FindAgentByIdIncludeSkillsAsync(int? id)
+        {
+            var agent = await context.Agents.Include(agent => agent.AgentToSkills).FirstOrDefaultAsync(agent => agent.Id == id);
+            return agent;
+        }
+
         public IQueryable<AgentToSkill> FindAgentToSkillForAgentById(int agentId)
         {
             IQueryable<AgentToSkill> agentToSkills = context.AgentToSkills.Include(agentToSkill => agentToSkill.Skill).Where(agentToSkill => agentToSkill.AgentId == agentId);
             return agentToSkills;
         }
 
+        public async Task<List<AgentToSkill>> FindAgentToSkillForAgentByIdAsync(int? agentId)
+        {
+            List<AgentToSkill> agentToSkills = await context.AgentToSkills.Include(agentToSkill => agentToSkill.Skill).Where(agentToSkill => agentToSkill.AgentId == agentId).ToListAsync();
+            return agentToSkills;
+        }
+
         public IQueryable<Agent> FindAgentsByLogin(string login)
         {
             IQueryable<Agent> agents = context.Agents.Where(ag => ag.Login == login && ag.IsActive == true);
+            return agents;
+        }
+
+        public async Task<List<Agent>> FindAgentsByLoginAsync(string login)
+        {
+            List<Agent> agents = await context.Agents.Where(ag => ag.Login == login && ag.IsActive == true).ToListAsync();
             return agents;
         }
 
@@ -109,14 +170,34 @@ namespace ControlPanel.Concrete
             }
         }
 
+        public async Task DeleteAsync(int id)
+        {
+            var agent = await context.Agents.FindAsync(id);
+            if(agent!=null)
+            {
+                context.Agents.Remove(agent);
+            }
+        }
+
         public void Save()
         {
             context.SaveChanges();
         }
 
+        public async Task SaveAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
         public IQueryable<Agent> SearchAgent(string searchString)
         {
             var agents = context.Agents.Where(ag => ag.Login.Contains(searchString) && ag.IsActive == true);
+            return agents;
+        }
+
+        public async Task<List<Agent>> SearchAgentsAsync(string searchString)
+        {
+            var agents = await context.Agents.Where(ag => ag.Login.Contains(searchString) && ag.IsActive == true).ToListAsync();
             return agents;
         }
 
@@ -134,9 +215,24 @@ namespace ControlPanel.Concrete
             }
         }
 
+        public async Task DeleteAgentToSkillAsync(int id)
+        {
+            var agentToSkill = await context.AgentToSkills.FindAsync(id);
+            if(agentToSkill!=null)
+            {
+                context.AgentToSkills.Remove(agentToSkill);
+            }
+        }
+
         public IQueryable<AgentToSkill> FindAgentToSkills(int agentId, int skillId)
         {
             IQueryable<AgentToSkill> agentToSkills = context.AgentToSkills.Where(agToSkill => agToSkill.AgentId == agentId && agToSkill.SkillId == skillId);
+            return agentToSkills;
+        }
+
+        public async Task<List<AgentToSkill>> FindAgentToSkillsAsync(int agentId, int skillId)
+        {
+            List<AgentToSkill> agentToSkills = await context.AgentToSkills.Where(agToSkill => agToSkill.AgentId == agentId && agToSkill.SkillId == skillId).ToListAsync();
             return agentToSkills;
         }
     }
