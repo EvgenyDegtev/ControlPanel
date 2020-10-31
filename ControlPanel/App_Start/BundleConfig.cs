05149714000f989821ae63159c141295bf8de9e3
edegtev <evgeny89degtev@mail.ru>
2020-10-31 21:31:17 +0300

    Add navigation controller

--- ControlPanel/App_Start/BundleConfig.cs
+++ ControlPanel/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ControlPanel
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
+                      "~/Content/Site.css",
                       "~/Content/PagedList.css"));
 
             bundles.Add(new StyleBundle("~/Content/fontawesome").Include(
---
+++ ControlPanel/Controllers/NavigationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlPanel.Controllers
+{
+    public class NavigationController : Controller
+    {
+        // GET: Navigation
+        public PartialViewResult Menu()
+        {
+            ViewBag.SelectedCategory=RouteData.Values["controller"];
+            return PartialView();
+        }
+    }
+}
