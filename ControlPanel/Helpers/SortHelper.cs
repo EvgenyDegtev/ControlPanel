58d55b68ae816c050f31b933465bca158e049757
edegtev <evgeny89degtev@mail.ru>
2020-11-13 23:58:13 +0300

    Add SortHelper

---
+++ ControlPanel/Helpers/SortHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlPanel.Helpers
+{
+    public static class SortHelper
+    {
+        public static MvcHtmlString Sort(this HtmlHelper html, string sortOrder="desc")
+        {
+            //< i class="sort fa fa-sort-amount-desc"></i>
+            TagBuilder spanTag = new TagBuilder("span");
+            TagBuilder iTag = new TagBuilder("i");
+            
+            if (sortOrder == "asc")
+            {
+                iTag.MergeAttribute("class", "fa fa-sort-amount-asc");
+            }
+            else
+            {
+                iTag.MergeAttribute("class", "fa fa-sort-amount-desc");
+            }
+            spanTag.InnerHtml += iTag.ToString();
+
+            return new MvcHtmlString(spanTag.ToString());
+        }
+    }
+}
