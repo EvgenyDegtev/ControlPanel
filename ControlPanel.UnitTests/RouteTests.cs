6b754106ac904805a81e4bbeba008ea95893d1ee
edegtev <evgeny89degtev@mail.ru>
2020-11-22 15:27:07 +0300

    Add routing test for Skills, Routes,Groups,Reports

--- ControlPanel.UnitTests/RouteTests.cs
+++ ControlPanel.UnitTests/RouteTests.cs
@@ -85,12 +85,13 @@ namespace ControlPanel.UnitTests
         public void TestAgentsIncomingRoutes()
         {
             TestRouteMatch("~/", "Agents", "Index");
+            TestRouteMatch("~/Agents", "Agents", "Index");
             //TestRouteMatch("~/?page=2", "Agents", "Index");
+            TestRouteMatch("~/Agents/Index", "Agents", "Index");
             TestRouteMatch("~/Agents/Index/?page=2", "Agents", "Index");
             TestRouteMatch("~/Agents/Index/?page=2&searchString=1", "Agents", "Index");
-            TestRouteMatch("~/Agents/Index", "Agents", "Index");
             TestRouteMatch("~/Agents/Create","Agents","Create");
-            TestRouteMatch("~/Agents/Edit/29", "Agents", "Edit");
+            TestRouteMatch("~/Agents/Edit/22", "Agents", "Edit");
             TestRouteMatch("~/Agents/Delete/29", "Agents", "Delete");
             TestRouteMatch("~/Agents/22/Skills", "Agents", "AgentSkills");
             TestRouteMatch("~/Agents/22/EditSkill/4", "Agents", "EditSkill");
@@ -102,19 +103,47 @@ namespace ControlPanel.UnitTests
         [TestMethod]
         public void TestSkillIncomingRoutes()
         {
-
+            TestRouteMatch("~/Skills", "Skills", "Index");
+            TestRouteMatch("~/Skills/Index", "Skills", "Index");
+            TestRouteMatch("~/Skills/Index/?page=2", "Skills", "Index");
+            TestRouteMatch("~/Skills/Index/?page=2&searchString=1", "Skills", "Index");
+            TestRouteMatch("~/Skills/Create", "Skills", "Create");
+            TestRouteMatch("~/Skills/Edit/29", "Skills", "Edit");
+            TestRouteMatch("~/Skills/Delete/29", "Skills", "Delete");
+            TestRouteMatch("~/Skills/4/Routes", "Skills", "SkillRoutes");
         }
 
         [TestMethod]
         public void TestGroupIncomingRoutes()
         {
-
+            TestRouteMatch("~/Groups", "Groups", "Index");
+            TestRouteMatch("~/Groups/Index", "Groups", "Index");
+            TestRouteMatch("~/Groups/Index/?page=2", "Groups", "Index");
+            TestRouteMatch("~/Groups/Index/?page=2&searchString=1", "Groups", "Index");
+            TestRouteMatch("~/Groups/Create", "Groups", "Create");
+            TestRouteMatch("~/Groups/Edit/29", "Groups", "Edit");
+            TestRouteMatch("~/Groups/Delete/29", "Groups", "Delete");
+            TestRouteMatch("~/Groups/14/Agents", "Groups", "GroupAgents");
+            TestRouteMatch("~/Groups/14/RemoveAgent/55", "Groups", "RemoveAgent");
         }
 
         [TestMethod]
         public void TestRouteIncomingRoutes()
         {
+            TestRouteMatch("~/Routes", "Routes", "Index");
+            TestRouteMatch("~/Routes/Index", "Routes", "Index");
+            TestRouteMatch("~/Routes/Index/?page=2", "Routes", "Index");
+            TestRouteMatch("~/Routes/Index/?page=2&searchString=1", "Routes", "Index");
+            TestRouteMatch("~/Routes/Create", "Routes", "Create");
+            TestRouteMatch("~/Routes/Edit/29", "Routes", "Edit");
+            TestRouteMatch("~/Routes/Delete/29", "Routes", "Delete");
+        }
 
+        [TestMethod]
+        public void TestReportIncomingRoutes()
+        {
+            TestRouteMatch("~/Reports", "Reports", "Index");
+            TestRouteMatch("~/Reports/Index", "Reports", "Index");
         }
     }
 }
