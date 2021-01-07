98dbeab49772211d997c261acb0250483156c764
edegtev <evgeny89degtev@mail.ru>
2021-01-07 19:28:21 +0300

    Add monitoring controller base version

---
+++ ControlPanel/Controllers/MonitoringController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using NLog;
+
+namespace ControlPanel.Controllers
+{
+    [Authorize]
+    public class MonitoringController: Controller
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        [HttpGet]
+        public ActionResult TopAgents ()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> TopSkills()
+        {
+            return View();
+        }
+
+    }
+}
